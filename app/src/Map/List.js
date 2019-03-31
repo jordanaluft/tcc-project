a244653e56d289313f9b912fd1edefd748331e0d
Jordana Luft <jordana.sluft@gmail.com>
2019-03-31 16:02:48 -0300

    Criado metodo get do servidor

---
+++ app/src/Map/List.js
@@ -0,0 +1,33 @@
+import React from "react";
+import axios from "axios";
+
+class List extends React.Component {
+  state = {
+    lampadas: []
+  };
+
+  getData = () => {
+    axios.get("http://localhost:8000/lampadas").then(response => {
+      console.log(response);
+      this.setState({ lampadas: response.data });
+    });
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.lampadas.map(lampada => (
+          <li key={lampada.id}>
+            {lampada.id} {lampada.latitude} {lampada.longitude}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default List;
--- app/src/Map/Map.js
+++ app/src/Map/Map.js
@@ -1,28 +1,10 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
-import Grid from "@material-ui/core/Grid";
+import List from "./List";
 
-const mapStyles = {
-  width: "60%",
-  height: "60%"
-};
+function Map(props) {
+  const { classes } = props;
 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233
-        }}
-      />
-    );
-  }
+  return <List />;
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyBWFGtzS11m78M0ILPTHBEtRcI-RKQ6nUY"
-})(MapContainer);
+export default Map;

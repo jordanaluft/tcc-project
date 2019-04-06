ff2ab0d8e91f06150019dc2b2ee841e5134fe7f6
Jordana Luft <jordana.sluft@gmail.com>
2019-04-06 19:17:58 -0300

    implementada localização automatica

--- app/src/New/New.js
+++ app/src/New/New.js
@@ -1,14 +1,15 @@
-import React from "react";
-import axios from "axios";
-import PropTypes from "prop-types";
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 import {
   Paper,
   Grid,
   Typography,
   withStyles,
   Input,
-  Button
-} from "@material-ui/core";
+  Button,
+  TextField,
+} from '@material-ui/core';
 
 const styles = theme => ({
   root: {
@@ -18,62 +19,62 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3,
     marginLeft: theme.spacing.unit * 5,
     minWidth: 300,
-    width: "35%"
+    width: '35%',
   },
   input: {
-    marginLeft: theme.spacing.unit
+    marginLeft: theme.spacing.unit,
   },
   button: {
-    margin: theme.spacing.unit
-  }
+    margin: theme.spacing.unit,
+  },
 });
 
 class New extends React.Component {
   state = {
-    id: "",
-    intensity: "",
-    last_ping: "",
-    status: "",
-    latitude: "",
-    longitude: "",
+    id: '',
+    intensity: '',
+    last_ping: '',
+    status: '',
+    latitude: '',
+    longitude: '',
     isLoadign: false,
-    hasError: false
+    hasError: false,
   };
 
   sendData = () => {
     this.setState(
       {
         isLoading: true,
-        hasError: false
+        hasError: false,
       },
       () =>
         axios
-          .post("http://localhost:8000/lampadas", {
+          .post('http://localhost:8000/lampadas', {
             id: this.state.id,
             intensity: 123,
-            last_ping: "0123-12-23T15:12:00-03:06",
-            status: "broken",
-            latitude: 321321.0,
-            longitude: 32321.0
+            last_ping: '0123-12-23T15:12:00-03:06',
+            status: 'broken',
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
           })
           .then(() =>
             this.setState({
               isLoading: false,
               hasError: false,
-              id: "",
-              itensity: "",
-              last_ping: "",
-              status: "",
-              latitude: "",
-              longitude: ""
-            })
+              id: '',
+              itensity: '',
+              last_ping: '',
+              status: '',
+              latitude: '',
+              longitude: '',
+            }),
           )
           .catch(() =>
             this.setState({
               isLoading: false,
-              hasError: true
-            })
-          )
+              hasError: true,
+            }),
+          ),
     );
   };
 
@@ -104,7 +105,7 @@ class New extends React.Component {
                   value={this.state.id}
                   onChange={e => this.setState({ id: e.target.value })}
                   inputProps={{
-                    "aria-label": "Description"
+                    'aria-label': 'Description',
                   }}
                 />
               </Typography>
@@ -117,7 +118,7 @@ class New extends React.Component {
                   value={this.state.latitude}
                   onChange={e => this.setState({ latitude: e.target.value })}
                   inputProps={{
-                    "aria-label": "Description"
+                    'aria-label': 'Description',
                   }}
                 />
               </Typography>
@@ -130,11 +131,25 @@ class New extends React.Component {
                   value={this.state.longitude}
                   onChange={e => this.setState({ longitude: e.target.value })}
                   inputProps={{
-                    "aria-label": "Description"
+                    'aria-label': 'Description',
                   }}
                 />
               </Typography>
               <p />
+              <Button
+                variant="contained"
+                className={classes.button}
+                onClick={() =>
+                  navigator.geolocation.getCurrentPosition(position =>
+                    this.setState({
+                      latitude: position.coords.latitude,
+                      longitude: position.coords.longitude,
+                    }),
+                  )
+                }
+              >
+                Find my Location!
+              </Button>
               <Button
                 variant="contained"
                 className={classes.button}
@@ -150,7 +165,7 @@ class New extends React.Component {
   }
 }
 New.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(New);
